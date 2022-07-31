3e6a27394397db269f984920cccbe9e325d589dc
Tatianabz <bztn@yandex.ru>
2022-07-31 14:16:39 +0700

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check( str, bracketsConfig) {
+  let strArr = str.split('');
+  let flag = true;
+  let num;
+  while (flag == true ) {
+      flag = false;
+    for (let arr of bracketsConfig) {
+         for (let i = 0; i < strArr.length-1; i++) {
+        if (arr[0] == strArr[i] && arr[1] == strArr[i + 1]) {
+          num = i;
+          strArr.splice(num, 2);
+          flag = true;
+          if ( strArr.length == 0){
+            return true;
+          }   if ( strArr.length == 1){
+            return false;
+          }
+        }
+      }
+    }
+  }
+  return flag;
+};
